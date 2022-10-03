3eda08872b89cb02bd63ccab060f4f2ac038e447
Md. Abu Musa <musa.fci@gmail.com>
2022-10-03 23:21:10 +0600

    Commit#2 Booking History

--- resources/js/components/booking/BookingHistory.vue
+++ resources/js/components/booking/BookingHistory.vue
@@ -1,7 +1,59 @@
 <template>
-  <div>
-    asdf as asdf ad
-  </div>
+  <section class="content">
+    <div class="container-fluid">
+        <div class="row">
+
+          <div class="col-12">
+        
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Booking History</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Category</th>
+                      <th>Price</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+
+                      <td>11</td>
+                      <td>asdfa</td>
+                      <td>sadf asdfa</td>
+                      <td>sdfa</td>
+                      <td>sdfas</td>
+                      <td>
+                        
+                        <a href="#">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                  <pagination></pagination>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+    </div>
+  </section>
 </template>
 
 <script>
